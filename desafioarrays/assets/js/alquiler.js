58c24c44cfa580dc638b7b855fc4e872ca27b15d
Giselle Thourey <149951283+gisellethourey@users.noreply.github.com>
2024-02-04 23:07:01 -0300

    Add files via upload

---
+++ desafioarrays/assets/js/alquiler.js
@@ -0,0 +1,36 @@
+import propiedadesAlquiler from "./propiedades_alquiler.js"
+const mostrarPropiedadesAlquiler = () => {
+    const container = document.getElementById("soloPropiedadesAlquiler")
+    propiedadesAlquiler.forEach((propiedad)=> {
+        const card = document.createElement("div")
+        card.className= "col-mb-a mb-4"
+        card.innerHTML = `
+        <div class ="card">
+        <img
+        src ="${propiedad.src}"
+        class= "card-img-top"
+        alt ="Imagen de la propiedad"
+        />
+        <div class ="card-body">
+        <h5 class= "card-title">${propiedad.nombre}</h5>
+       <p class= "card-text">${propiedad.descripcion}</p> 
+       <p>
+       <i class = "fas fa-bed"></i> ${propiedad.habitaciones}
+       <i class = "fas fa-bath"></i> ${propiedad.baños} <br> 
+       <i class = "fas fa-dollar-sign"></i> ${propiedad.costo} <br>
+      
+       <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+ 
+             ${propiedad.smoke ? 
+                     `<p class="text-success"><i class="fas fa-smoking"></i> Se permite fumar</p>` : 
+                     `<p class="text-danger"><i class="fas fa-smoking-ban"></i> No se permite fumar</p>`}
+                 ${propiedad.pets ? 
+                     `<p class="text-success"><i class="fas fa-paw"></i> Se permiten mascotas</p>` : 
+                     `<p class="text-danger"><i class="fas fa-ban"></i> No se permiten mascotas</p>`}
+        </div>
+        </div> 
+        `;
+        container.appendChild(card)
+    })
+};
+mostrarPropiedadesAlquiler()
